4c5b08d951dc4cd6500d31c2b5cabbcb3ab60ffe
Misslin Quentin <misslin.quentin@gmail.com>
2022-04-22 12:37:07 +0200

    Added a 'web menu' class inherited from 'web browser'.

---
+++ Source/ChromiumUI/Private/WebMenu.cpp
@@ -0,0 +1,76 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "WebMenu.h"
+
+#define LOCTEXT_NAMESPACE "WebBrowser"
+
+/////////////////////////////////////////////////////
+// UWebBrowser
+
+UWebMenu::UWebMenu(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	Zoom = 1.0f;
+}
+
+void UWebMenu::SendJson(const FString& Json)
+{
+	if (WebBrowserWidget.IsValid())
+	{
+		const FString js = "if(window && window.Unreal && window.Unreal.receive) window.Unreal.receive(`" + Json + "`);";
+		UWebMenu::ExecuteJavascript(js);
+	}
+}
+
+float UWebMenu::GetZoom() const
+{
+	return Zoom;
+}
+
+bool UWebMenu::IsValid() const
+{
+	return WebBrowserWidget.IsValid();
+}
+
+void UWebMenu::ReceiveJson(FString data) const
+{
+	if (WebBrowserWidget.IsValid())
+	{
+		OnJsonReceived.Broadcast(FText::FromString(data));
+	}
+}
+
+TSharedRef<SWidget> UWebMenu::RebuildWidget()
+{
+	if ( IsDesignTime() )
+	{
+		return SNew(SBox)
+			.HAlign(HAlign_Center)
+			.VAlign(VAlign_Center)
+			[
+				SNew(STextBlock)
+				.Text(LOCTEXT("Web Menu", "Web Menu"))
+			];
+	}
+	else
+	{
+		WebBrowserWidget = SNew(SWebBrowser)
+			.InitialURL(InitialURL)
+			.ShowControls(false)
+			.SupportsTransparency(bSupportsTransparency)
+			.EnableMouseTransparency(bEnableMouseTransparency)
+			.TransparencyDelay(MouseTransparencyDelay)
+			.TransparencyThreshold(MouseTransparencyThreshold)
+			.EnableVirtualPointerTransparency(bEnableVirtualPointerTransparency)
+			.OnUrlChanged(BIND_UOBJECT_DELEGATE(FOnTextChanged, HandleOnUrlChanged))
+			.OnBeforePopup(BIND_UOBJECT_DELEGATE(FOnBeforePopupDelegate, HandleOnBeforePopup));
+
+#if WITH_CEF3
+		WebBrowserWidget->BindUObject("interface", this);
+#endif
+
+		return WebBrowserWidget.ToSharedRef();
+	}
+}
+
+#undef LOCTEXT_NAMESPACE
---
+++ Source/ChromiumUI/Public/WebMenu.h
@@ -0,0 +1,59 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "./WebBrowser.h"
+
+#include "WebMenu.generated.h"
+
+
+/**
+ * 
+ */
+UCLASS()
+class CHROMIUMUI_API UWebMenu : public UWebBrowser
+{
+	GENERATED_UCLASS_BODY()
+
+public:
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnJsonReceived, const FText&, Text);
+
+	/**
+	* Send an event to web browser with Json
+	*
+	* @param Json Json string to execute
+	*/
+	UFUNCTION(BlueprintCallable, Category = "Web Browser")
+		void SendJson(const FString& Json);
+
+	/**
+	* Get resolution from webbrowser
+	*
+	* @param Json Json string to execute
+	*/
+	UFUNCTION(BlueprintCallable, Category = "Web Browser")
+		float GetZoom() const;
+
+	/**
+	* Check if web Browser is valid
+	*/
+	UFUNCTION(BlueprintCallable, Category = "Web Browser")
+		bool IsValid() const;
+
+	/**
+	* Called by web Browser to send json to Unreal
+	*/
+	UFUNCTION(BlueprintCallable, Category = "Web Browser")
+		void ReceiveJson(FString json) const;
+
+	/** Called when json received from client. */
+	UPROPERTY(BlueprintAssignable, Category = "Web Browser|Event")
+		FOnJsonReceived OnJsonReceived;
+
+	/** Set zoom of page */
+	UPROPERTY(BlueprintReadWrite, Category = "Web Browser|Appearance")
+		float Zoom;
+
+protected:
+	virtual TSharedRef<SWidget> RebuildWidget() override;
+};
